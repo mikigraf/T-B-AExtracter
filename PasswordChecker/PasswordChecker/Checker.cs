f4aa94f6f05795bcceb965857cdd53cff17875c5
ミキ Wawrzyniak <mikolajspejs@gmail.com>
2016-08-13 23:49:33 +0200

    initial commit with basic functionality

---
+++ PasswordChecker/PasswordChecker/Checker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PasswordChecker
+{
+    class Checker
+    {
+        [DllImport("kernel32.dll")]
+        public static extern Int32 ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesRead);
+
+        public static byte[] ReadBytes(IntPtr Handle, Int64 Address, uint BytesToRead)
+        {
+            IntPtr ptrBytesRead;
+            byte[] buffer = new byte[BytesToRead];
+            ReadProcessMemory(Handle, new IntPtr(Address), buffer, BytesToRead, out ptrBytesRead);
+            return buffer;
+        }
+
+        public static string ReadString(Int64 Address, IntPtr Handle)
+        {
+            byte[] bytes = ReadBytes(Handle, Address, 16);
+            string s = Encoding.ASCII.GetString(bytes);
+            return s.Replace("-", "");
+        }
+
+        public string ReadString(long Address, IntPtr Handle, uint length = 32)
+        {
+            return ASCIIEncoding.Default.GetString(ReadBytes(Handle, Address, length)).Split('\0')[0];
+        }
+
+        public string findPassword(UInt32 Base, IntPtr Handle)
+        {
+            return ReadString(Base + 0x6D6024, Handle);
+        }
+
+        public string findLogin(UInt32 Base, IntPtr Handle)
+        {
+            return ReadString(Base + 0x6D600C, Handle);
+        }
+    }
+}
---
+++ PasswordChecker/PasswordChecker/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PasswordChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.check = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(12, 12);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(535, 29);
+            this.login.TabIndex = 0;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(12, 58);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(535, 29);
+            this.password.TabIndex = 1;
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(12, 103);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(535, 77);
+            this.check.TabIndex = 2;
+            this.check.Text = "Check!";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 192);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button check;
+    }
+}
+
